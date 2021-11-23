9cc7aab4ac943908ef9c461bc51c0793ac61ce5b
Diego Durán <diego.duran.rguez@gmail.com>
2021-11-24 00:02:50 +0100

    fix not found elements

--- src/controller/showModuleTitle.controller.ts
+++ src/controller/showModuleTitle.controller.ts
@@ -3,12 +3,18 @@ import { Page } from 'puppeteer';
 
 const showModuleTitle = async (page: Page) => {
   const title = await page.evaluate(async () => {
-    return document
-      .getElementsByTagName('h1')[0].innerText;
+    const heading = document
+      .getElementsByTagName('h1');
+    return (heading.length)
+      ? heading[0].innerText
+      : undefined;
   });
   const subTitle = await page.evaluate(async () => {
-    return document
-      .getElementsByTagName('h2')[0].innerText;
+    const heading = document
+      .getElementsByTagName('h2');
+    return (heading.length)
+      ? heading[0].innerText
+      : undefined;
   });
   logger({severity: 'success', message: `Showing module '${title}' > '${subTitle}'`});
 }
--- src/controller/stepAction.controller.ts
+++ src/controller/stepAction.controller.ts
@@ -2,10 +2,40 @@ import { logger } from '../util/logger';
 import { Page } from 'puppeteer';
 
 const stepAction = async (page: Page, action: 'previous'|'next') => {
+  const existsPrevButton = await page.evaluate(async () => {
+    return !!document
+      .getElementsByClassName('module-sequence-footer-button--previous')
+      .length;
+  });
+  const existsNextButton = await page.evaluate(async () => {
+    return !!document
+      .getElementsByClassName('module-sequence-footer-button--next')
+      .length;
+  });
+
   const classname = '.module-sequence-footer-button--';
   await page.waitForSelector('.module-sequence-footer', { visible: true });
-  logger({severity: 'info', message: `Clicking the ${action} button`});
-  await page.click(`${classname}${action} > a`);
+  if (existsPrevButton && action === 'previous' || existsNextButton && action === 'next') {
+    logger({ severity: 'info', message: `Clicking the ${action} button` });
+    await page.click(`${classname}${action} > a`);
+  } else {
+    if (existsPrevButton) {
+      logger({
+        severity: 'warning',
+        message: `No ${action} button found, clicking on previous button`
+      });
+      await page.click(`${classname}previous > a`);
+    } else if (existsNextButton) {
+      logger({
+        severity: 'warning',
+        message: `No ${action} button found, clicking on next button`
+      });
+      await page.click(`${classname}next > a`);
+    } else {
+      logger({ severity: 'error', message: `No ${action} button found` });
+      throw new Error(`No ${action} button found`);
+    }
+  }
   await page.waitForNavigation();
 }
 
